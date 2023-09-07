11592e14965571749a0ba3562944078a8311ab9f
spencer kelly <spencermountain@gmail.com>
2023-09-07 10:50:16 -0400

    working in sequence

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import fetch from 'node-fetch'
 import knownServer from './01-knownServer.js'
 import parsers from './02-parseHtml.js'
 import wget from './_wget.js'
+import slow from 'slow'
 import { URL } from 'url'
 import path from 'path'
+import fs from 'fs'
 
-const getPage = async function (url) {
+const getPage = async function (url, dir) {
   let req = await fetch(url)
   let html = await req.text()
   let res = { files: [], dirs: [] }
@@ -27,30 +29,38 @@ const getPage = async function (url) {
   // make them absolute links
   res.files.forEach((obj) => {
     obj.href = new URL(obj.href, url).href
+    obj.local = path.join(dir, obj.name)
+    obj.dir = dir
   })
   res.dirs.forEach((obj) => {
     obj.href = new URL(obj.href, url).href
+    obj.local = path.join(dir, obj.name)
   })
   // filter down any files
   return res
 }
 
-// downloan any files
-const getFiles = async function (res, dir) {
-  console.log(`getting ${res.files.length} files`)
+const getFile = async function (obj) {
+  let { local, dir, href } = res.files[i]
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir)
+    console.log('making: ' + dir)
   }
-  for (let i = 0; i < res.files.length; i += 1) {
-    let file = path.join(dir, res.files[i].name)
-    await wget(res.files[i].href, file)
-  }
+  await wget(href, local)
+}
+
+// downloan any files
+const getFiles = async function (res) {
+  console.log(`getting ${res.files.length} files`)
+  await slow.walk(res.files, getFile)
 }
 
 const getDirs = async function (res) {
   console.log(`getting ${res.dirs.length} dirs`)
   for (let i = 0; i < res.dirs.length; i += 1) {
-    let r = await getPage(res.dirs[i].href)
+    let { href, local } = res.dirs[i]
+    // add new data to res
+    let r = await getPage(href, local)
     console.log(`+ ${r.dirs.length} dirs, ${r.files.length} files`)
     res.files = res.files.concat(r.files)
     res.dirs = res.dirs.concat(r.dirs)
@@ -59,11 +69,11 @@ const getDirs = async function (res) {
 }
 
 const remoteWork = async function (url, dir, opts) {
-  let res = await getPage(url)
+  let res = await getPage(url, dir)
   let times = 0
   while (res.files.length || res.dirs.length) {
-    console.log('#' + times, res.dirs.length, res.files.length)
-    await getFiles(res, dir)
+    // console.log('#' + times, res.dirs.length, res.files.length)
+    await getFiles(res)
     res.files = []
 
     await getDirs(res)
